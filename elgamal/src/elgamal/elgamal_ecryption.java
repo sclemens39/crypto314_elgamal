91db99ad17bcbf64f8998c86544afcc6792b9c08
scleme2 <scleme2@students.towson.edu>
2017-11-30 17:30:33 -0500

    Added bare bones

---
+++ elgamal/src/elgamal/elgamal_ecryption.java
@@ -0,0 +1,21 @@
+package elgamal;
+import java.util.*;
+import java.math.*;
+
+public class elgamal_ecryption {
+	
+	//p = large prime
+	//a = primitive root of p
+	//x = random int
+	//b = a^x (mod p) 
+	//k = key
+	
+	int p,a,b,k;
+	
+	int x = (int)Math.random()*100;
+	
+	void findPrim(int p){
+		// find primitive root of p
+		
+	}
+}
