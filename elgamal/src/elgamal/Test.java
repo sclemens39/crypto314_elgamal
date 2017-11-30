c805563171fa6ca90324d91244624e5db99f697d
scleme2 <scleme2@students.towson.edu>
2017-11-30 18:55:13 -0500

    Added large prime number generator and a few other methods to el gamal class

--- elgamal/src/elgamal/Test.java
+++ elgamal/src/elgamal/Test.java
@@ -1,5 +1,11 @@
 package elgamal;
+/*
+Solving the discrete log problem (using brute force)
+Computing the multiplicative inverse in modular arithmetic using the extended Euclidean algorithm
+Modular exponentiation using the repeated squaring method
+Converting from numbers back to text (Note: You need to invert the above procedure which converts from text to numbers).
 
+ */
 public class Test {
 
 }
--- elgamal/src/elgamal/elgamal_ecryption.java
+++ elgamal/src/elgamal/elgamal_ecryption.java
@@ -11,11 +11,42 @@ public class elgamal_ecryption {
 	//k = key
 	
 	int p,a,b,k;
-	
-	int x = (int)Math.random()*100;
-	
-	void findPrim(int p){
-		// find primitive root of p
+	Random rand = new Random();
+
+	int findPrim(int p){
+		// return prime root of random number (assume p)
+		int counter = 2;
 		
+		while(counter * counter < p){
+			if(p % counter == 0){
+				return counter;
+			} else {
+				counter++;
+			}
+		}
+		
+		return counter;
+	}
+	
+	boolean isPrime(int x){
+		if(x % 2 == 0){
+			return false;
+		}
+		return true;
+	}
+	
+	public int generateLargePrime(){
+		int largePrime = 3;
+		int  n = rand.nextInt(Integer.MAX_VALUE);
+		
+		//while int range is not exceeded multiply largePrime with random number
+		//until prime returned
+		while(largePrime <= Integer.MAX_VALUE && !isPrime(largePrime)){
+			largePrime = largePrime * n;
+			if(isPrime(largePrime)){
+				return largePrime;
+			}
+		}
+		return largePrime;
 	}
 }
