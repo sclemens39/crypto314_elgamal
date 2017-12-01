329c72ac055515486d3dad505af258667d9b4025
scleme2 <scleme2@students.towson.edu>
2017-12-01 18:42:55 -0500

    #

--- elgamal/src/elgamal/DLP.java
+++ elgamal/src/elgamal/DLP.java
@@ -1,5 +1,9 @@
 package elgamal;
-
+/*
+ * Goal: Find k such that a^k = y(mod q). (In other words, find the position of y
+ * in the huge list {a, a^2, a^3, ... ,a^(q-1)}.
+ * In short, DLP is: given a^x (mod q), find x.
+ */
 public class DLP {
 	//Input: q = prime number; a = primitive root of q;
 	//		 y = a residue mod q
@@ -7,4 +11,5 @@ public class DLP {
 	int k=0, x=0;
 	
 	//Use ExtendedEuclid class 
+	
 }
