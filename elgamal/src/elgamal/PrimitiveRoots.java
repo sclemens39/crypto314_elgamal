49a8bbe48e636d3fe6a4b4cbbbb63420ab98bfa4
ArcanaXIV <33043311+ArcanaXIV@users.noreply.github.com>
2017-12-05 20:08:57 -0500

    Add files via upload

---
+++ elgamal/src/elgamal/PrimitiveRoots.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elgamal;
+import java.lang.Math;
+/**
+ *
+ * @author ltder
+ */
+public class PrimitiveRoots {
+    
+    public int primRoot(int p){
+        double k;
+        int o =1;
+        int z = 0;
+        double[] roots= new double[100000];
+        for (int r = 2; r < p; r++) {
+		k = Math.pow(r, o);
+		k %= p;
+		while (k > 1) {
+			o++;
+			k *= r;
+			k %= p;
+		}
+		if (o == (p - 1)) {
+			//form.resultbox.value += (r + " ");
+			//alert(r + " is a primitive root.");
+			roots[z] = r;
+			z++;
+		}
+		o = 1;
+	}
+
+	z--;
+	
+	for (int y = 0; y < z; y++) {
+		System.out.println(roots[y] + ", ");
+	}
+	
+	System.out.println("and " + roots[z] + ".");
+        return -1;
+}
+}
