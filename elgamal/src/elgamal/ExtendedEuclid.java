2dbe63b277845e9977aedf6e7176d06a2d554d75
scleme2 <scleme2@students.towson.edu>
2017-12-07 17:45:29 -0500

    fixed merge

--- elgamal/src/elgamal/ExtendedEuclid.java
+++ elgamal/src/elgamal/ExtendedEuclid.java
@@ -1,15 +1,49 @@
 package elgamal;
 
 public class ExtendedEuclid {
-	   //  return array [d, a, b] such that d = gcd(p, q), ap + bq = d
-	   static int[] gcd(int p, int q) {
-	      if (q == 0)
-	         return new int[] { p, 1, 0 };
+	
+	//initialize variables
+	private int r = 1;
+	
+	public int extAlgorithm(int a, int b) {
+		int x = 0, temp = 0, y = 1;
+	
+		if( r == 0){
+			return b;
+		}
+		else {
+			r = 0;
+			while (b != 0 && temp != a) {
+	
+				//Section of code is responsible for finding the remainder value.
+				if (temp != a) {
+					y++;
+				if (b * y <= a && (a - (b * y)) < b) {
+					temp = (b * y) + r;
+						r = a - temp;
+				}
+				temp = (b * y) + r;
+			}
+		}
+		a = b;
+	
+		if(r == 0) {
+			return b;
+		}
+		b = r;
+		}
+		return extAlgorithm(a,b);
+	}
+	
+	// return array [d, a, b] such that d = gcd(p, q), ap + bq = d
+	static int[] gcd(int p, int q) {
+	    if (q == 0)
+	    	return new int[] { p, 1, 0 };
 
-	      int[] vals = gcd(q, p % q);
-	      int d = vals[0];
-	      int a = vals[2];
-	      int b = vals[1] - (p / q) * vals[2];
-	      return new int[] { d, a, b };
-	   }
+	    int[] vals = gcd(q, p % q);
+		int d = vals[0];
+	    int a = vals[2];
+	    int b = vals[1] - (p / q) * vals[2];
+	    return new int[] { d, a, b };
+	}
 }
--- elgamal/src/elgamal/Test.java
+++ elgamal/src/elgamal/Test.java
@@ -1,4 +1,8 @@
-package elgamal;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+//package elgamal;
 /*
 Solving the discrete log problem (using brute force)
 Computing the multiplicative inverse in modular arithmetic using the extended Euclidean algorithm
@@ -7,12 +11,40 @@ Converting from numbers back to text (Note: You need to invert the above procedu
 
  */
 public class Test {
-	
-	public static void main (String args[]){
+
+	public static void main (String args[]) throws FileNotFoundException{
+		Scanner scan = new Scanner(System.in);
+
 		PrimitiveRoots test = new PrimitiveRoots();
-		
-		test.primRoot(37);		
+		Encrypt encrypt = new Encrypt();
+		ExtendedEuclid ext = new ExtendedEuclid();
+
+		int a = 5, b = 18074, p = 31847;
+
+		System.out.println("B: " + ext.extAlgorithm(1180,482));
+
+		System.out.println("CipherText input: (r,t)");
+
+
 	}
+
+	/*void openFile(String fileName) throws FileNotFoundException {
+		Scanner x ;
+		File file = new File(fileName);
+		x = new Scanner(file);
+
+		int r,t;
+		try {
+
+			while(x.hasNext()) {
+				String line = x.nextLine();
+				String[] start = line.split("\\(");
+			}
+		}catch(FileNotFoundException e) {
+			System.out.println("File not found.");
+			e.printStackTrace();
+		}
+	}*/
 
 
 }
